90ead0e9e3fed72a7415a8f87d64f40f3440b27f
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-07 13:54:00 -0800

    more packages

--- R/packages.R
+++ R/packages.R
@@ -3,3 +3,17 @@
 
 library(conflicted)
 
+library(targets)
+library(renv)
+
+library(data.table)
+
+library(stantargets)
+library(cmdstanr)
+
+library(ggplot2)
+library(ggdist)
+library(patchwork)
+
+library(ggdag)
+library(dagitty)
