ffab9aada2c90779f21ecebffb98654d0b3aa630
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-07 16:12:43 -0800

    options, vars

--- _targets.R
+++ _targets.R
@@ -7,3 +7,30 @@
 lapply(dir('R', '*.R', full.names = TRUE), source)
 
 
+
+# Options -----------------------------------------------------------------
+# Targets
+tar_option_set(format = 'qs',
+               workspace_on_error = TRUE)
+
+# Stan
+options(mc.cores = 2,
+        scipen = 999,
+        digits = 2)
+
+
+
+# Variables ---------------------------------------------------------------
+# Stan model, compiled dir
+model_dir_stan <- file.path('stan')
+compiled_dir <- file.path('stan', 'compiled')
+output_stan_dir <- file.path('stan', 'output')
+stan_files <- dir(model_dir_stan, '.stan', full.names = TRUE)
+
+# cpp_options = list(stan_threads = TRUE),
+# chains = 4,
+# quiet = FALSE,
+# parallel_chains = 4,
+# threads_per_chain = 4,
+# dir = compiled_dir,
+# output_dir = output_stan_dir
