7f1d697517bcaea467504eb11c2508f2678f265b
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-11 13:35:54 -0800

    fst sim example

--- homework/helpers/simulation-example.R
+++ homework/helpers/simulation-example.R
@@ -56,3 +56,73 @@ m1 <- quap(
 )
 
 cbind(list(simulation = t(DT[1, .(intercept, slope_size, sigma)])), precis(m1))
+
+
+m2 <- quap(
+  alist(
+    km_hour ~ dnorm(mu, sigma),
+    mu <- intercept + slope_size * size,
+    intercept ~ dnorm(10, 100),
+    slope_size ~ dnorm(0, 100),
+    sigma ~ dexp(10)
+  ),
+  data = as.list(DT[, .(size, km_hour)])
+)
+
+cbind(list(simulation = t(DT[1, .(intercept, slope_size, sigma)])), precis(m2))
+
+
+m3 <- quap(
+  alist(
+    km_hour ~ dnorm(mu, sigma),
+    mu <- intercept + slope_size * size,
+    intercept ~ dnorm(100, 1000),
+    slope_size ~ dnorm(10, 1000),
+    sigma ~ dexp(100)
+  ),
+  data = as.list(DT[, .(size, km_hour)])
+)
+
+cbind(list(simulation = t(DT[1, .(intercept, slope_size, sigma)])), precis(m3))
+
+
+
+m4 <- quap(
+  alist(
+    km_hour ~ dnorm(mu, sigma),
+    mu <- intercept + slope_size * size,
+    intercept ~ dnorm(0, 1),
+    slope_size ~ dexp(1),
+    sigma ~ dnorm(0, 10)
+  ),
+  data = as.list(DT[, .(size, km_hour)])
+)
+
+# Error in quap(alist(km_hour ~ dnorm(mu, sigma), mu <- intercept + slope_size *  : 
+# initial value in 'vmmin' is not finite
+# The start values for the parameters were invalid. This could be caused by missing values (NA) in the data or by start values outside the parameter constraints. If there are no NA values in the data, try using explicit start values.
+
+m5 <- quap(
+  alist(
+    km_hour ~ dnorm(mu, sigma),
+    mu <- intercept + slope_size * size,
+    intercept ~ dnorm(10, 1),
+    slope_size ~ dexp(1),
+    sigma ~ dnorm(0, 10)
+  ),
+  data = as.list(DT[, .(size, km_hour)])
+)
+cbind(list(simulation = t(DT[1, .(intercept, slope_size, sigma)])), precis(m5))
+
+
+m6 <- quap(
+  alist(
+    km_hour ~ dnorm(mu, sigma),
+    mu <- intercept + slope_size * size,
+    intercept ~ dexp(1),
+    slope_size ~ dexp(1),
+    sigma ~ dexp(1)
+  ),
+  data = as.list(DT[, .(size, km_hour)])
+)
+cbind(list(simulation = t(DT[1, .(intercept, slope_size, sigma)])), precis(m6))
