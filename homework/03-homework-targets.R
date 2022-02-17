ffd3c775650adbddde56fb48c727bbffaf989da1
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-16 18:26:45 -0800

    add model sims q01

--- homework/03-homework-targets.R
+++ homework/03-homework-targets.R
@@ -1,13 +1,30 @@
 targets_homework_03 <- c(
   # Question 1 --------------------------------------------------------------
+  # Data
   tar_target(
     DT_h03_q01,
-    data_foxes(scaled = TRUE)
+    data_foxes(scale = TRUE)
   ),
+  
+  # Simulated
   tar_target(
     DT_sims_h03_q01,
     simulate_h03_q01()
   ),
+  
+  # Model simulation
+  tar_stan_mcmc(
+    h03_q01_sims,
+    file.path('stan', 'h03_q01.stan'),
+    list(
+      scale_food = DT_sims_h03_q01$scale_food,
+      scale_area = DT_sims_h03_q01$scale_area,
+      N = nrow(DT_sims_h03_q01)
+    ),
+    chains = 4,
+    dir = compiled_dir
+  ),
+  
   
   # Question 2 --------------------------------------------------------------
   
