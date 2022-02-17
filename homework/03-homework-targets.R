851cd44cac56837e3df3a252b3743d7970c76e60
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-16 18:14:07 -0800

    add data and sims data targets

--- homework/03-homework-targets.R
+++ homework/03-homework-targets.R
@@ -1,5 +1,13 @@
 targets_homework_03 <- c(
   # Question 1 --------------------------------------------------------------
+  tar_target(
+    DT_h03_q01,
+    data_foxes(scaled = TRUE)
+  ),
+  tar_target(
+    DT_sims_h03_q01,
+    simulate_h03_q01()
+  ),
   
   # Question 2 --------------------------------------------------------------
   
