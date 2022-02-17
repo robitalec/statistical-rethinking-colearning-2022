d9260c3f4d5957de8ba366971ddc76d4f2438ba8
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-16 18:14:00 -0800

    fst simulate h03 q01

---
+++ R/h03.R
@@ -0,0 +1,14 @@
+## Question 1
+#' Simulated data set to test model
+simulate_h03_q01 <- function() {
+  N_generate <- 1e3
+  sims <- data.table(
+    alpha = 0.3,
+    beta_area = 0.5,
+    sigma = 1,
+    area = runif(N_generate, 0.5, 10)
+  )
+  sims[, scale_area := as.numeric(scale(area))]
+  sims[, mu := alpha + beta_area * scale_area]
+  sims[, food := rnorm(.N, mu, sigma)]
+}
