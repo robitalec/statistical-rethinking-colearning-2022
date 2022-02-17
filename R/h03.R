bc06dcabd3b15d72bd2fc9181f4b863065ed7525
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-17 09:47:23 -0800

    plot new food predictions

--- R/h03.R
+++ R/h03.R
@@ -1,5 +1,5 @@
 ## Question 1
-#' Simulated data set to test model
+# Simulated data set to test model
 simulate_h03_q01 <- function() {
   N_generate <- 1e3
   sims <- data.table(
@@ -22,4 +22,13 @@ melt_fox_food_predictions <- function(draws, foxes) {
   melted[, new_food_index := as.integer(gsub('new_food\\[|\\]', '', new_food_index))]
   melted[, new_food := new_food[new_food_index]]
   return(melted)
+}
+
+# Plot new food predictions
+plot_fox_food_predictions <- function(predicted, observed) {
+  ggplot(predicted, aes(new_food, value)) + 
+    stat_lineribbon(color = NA) + 
+    geom_point(size = 2, aes(scale_food, scale_area), data = observed) +
+    theme_bw() + 
+    scale_fill_scico_d(alpha = 0.25, palette = 'acton', direction = -1, end = 0.8)
 }
