dbfe8ff8dc7e588cdd62e51d6c59422a900faf93
Alec L. Robitaille <robit.alec@gmail.com>
2022-02-17 13:04:13 -0800

    try LR

--- R/dag_plot.R
+++ R/dag_plot.R
@@ -12,7 +12,7 @@ dag_plot <- function(dag, output_path) {
   dag <- gsub('exposure', 'style=filled; color="#dfc9de"', dag)
   dag <- gsub('outcome', 'style=filled; color="#ffe0bd"', dag)
   dag <- gsub('dag', 'digraph', dag)
-  dag <- gsub('}', 'rankdir=TB}', dag)
+  dag <- gsub('}', 'rankdir=LR}', dag)
 
   dot_path <- paste0(output_path, '.dot')
   png_path <- paste0(output_path, '.png')
