086fd874e65a5d3c34b3627bbd4553e808280c27
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-04 16:15:26 -0800

    links

--- homework/04-homework-targets.R
+++ homework/04-homework-targets.R
@@ -1,6 +1,10 @@
 targets_homework_04 <- c(
   # Question 1 --------------------------------------------------------------
   
+  # LOO-PSIS: https://mc-stan.org/loo/reference/loo.html, https://mc-stan.org/loo/articles/loo2-with-rstan.html
+  # WAIC: https://mc-stan.org/loo/reference/waic.html
+  
+  
   # Render ------------------------------------------------------------------
   tar_render(render_h04,
              'homework/04-homework.Rmd')
