bdb9c2914e82501a2e109d2990f74c654bb05749
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-09 17:30:04 -0800

    dt marriage target

--- homework/04-homework-targets.R
+++ homework/04-homework-targets.R
@@ -1,5 +1,10 @@
 targets_homework_04 <- c(
   # Question 1 --------------------------------------------------------------
+  # Data
+  tar_target(
+    DT_marriage,
+    data_marriage()
+  ),
   
   # LOO-PSIS: https://mc-stan.org/loo/reference/loo.html, https://mc-stan.org/loo/articles/loo2-with-rstan.html
   # WAIC: https://mc-stan.org/loo/reference/waic.html
