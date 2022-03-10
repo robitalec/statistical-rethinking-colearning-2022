af15c3f2f4729ed2c1db0e7e57ebd96928b0fe66
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-09 17:33:47 -0800

    add model target

--- homework/04-homework-targets.R
+++ homework/04-homework-targets.R
@@ -9,6 +9,16 @@ targets_homework_04 <- c(
   # LOO-PSIS: https://mc-stan.org/loo/reference/loo.html, https://mc-stan.org/loo/articles/loo2-with-rstan.html
   # WAIC: https://mc-stan.org/loo/reference/waic.html
   
+  # Model
+  tar_mcmc(
+    h04_q01_m609,
+    file.path('stan', 'h03_q01.stan'),
+    c(N = DT_marriage[, .N],
+      N_index_married[, uniqueN(index_married)],
+      as.list(DT_marriage)),
+    chains = 4,
+    dir = compiled_dir
+  ),
   
   # Render ------------------------------------------------------------------
   tar_render(render_h04,
