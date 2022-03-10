b3b2ad529efd33764d75ccea08f587eced0bfe31
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-09 18:59:34 -0800

    fix arg

--- R/compare_loo.R
+++ R/compare_loo.R
@@ -1,5 +1,5 @@
 # LOO-PSIS: https://mc-stan.org/loo/reference/loo.html, https://mc-stan.org/loo/articles/loo2-with-rstan.html
 compare_loo <- function(models) {
-  ls_loo <- lapply(models, function(x) m$loo())
+  ls_loo <- lapply(models, function(m) m$loo())
   loo_compare(ls_loo)
 }
