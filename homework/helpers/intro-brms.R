f067c11cd7b127474697b78d8b35bcd71267e77c
Alec L. Robitaille <robit.alec@gmail.com>
2022-03-12 13:46:08 -0800

    add priors, sample priors

--- homework/helpers/intro-brms.R
+++ homework/helpers/intro-brms.R
@@ -38,3 +38,20 @@ DT[, index_island := .GRP, by = island]
 
 
 # Priors ------------------------------------------------------------------
+# Read more here: https://paul-buerkner.github.io/brms/reference/brmsformula.html
+f <- brmsformula(scaled_bill_length ~ scaled_body_mass[index_island])
+
+default_priors <- get_prior(f, data = DT)
+
+priors <- c(
+  prior(normal(0, 0.5), class = 'b'),
+  prior(normal(0, 0.2), class = 'Intercept'),
+  prior(exponential(1), class = 'sigma')
+)
+
+sample_priors <- brm(
+  formula = f,
+  data = DT,
+  prior = priors,
+  sample_prior = 'only'
+)
