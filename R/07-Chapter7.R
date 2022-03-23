98b480dc46752c8dd3404a76404164ce0c283f4c
icrichmond <icrichmond@mun.ca>
2022-03-23 12:44:48 -0400

    finished ch 7 (but didn't include all code)

--- R/07-Chapter7.R
+++ R/07-Chapter7.R
@@ -91,3 +91,32 @@ plot( brain_std ~ mass_std , data=d )
 lines( mass_seq , mu )
 shade( ci , mass_seq )
 
+# log-pointwise-predictive-density 
+set.seed(1)
+lppd(m7.1, n=1e4)
+
+sapply(list(m7.1, m7.2, m7.3, m7.4, m7.5, m7.6), function(m) sum(lppd(m)))
+
+# simulated training and testing 
+N <- 20 
+kseq <- 1:5 
+dev <- sapply(kseq, function(k) {
+  print(k);
+  r <- replicate(1e4, sim_train_test(N=N, k=k));
+  c(mean(r[1,]), mean(r[2,]), sd(r[1,]), sd(r[2,]))
+})
+
+
+
+plot( 1:5 , dev[1,] , ylim=c( min(dev[1:2,])-5 , max(dev[1:2,])+10 ) ,
+      xlim=c(1,5.1) , xlab="number of parameters" , ylab="deviance" ,
+      pch=16 , col=rangi2 )
+mtext( concat( 'N = ',N ) )
+points( (1:5)+0.1 , dev[2,] )
+for ( i in kseq ) {
+  pts_in <- dev[1,i] + c(-1,+1)*dev[3,i]
+  pts_out <- dev[2,i] + c(-1,+1)*dev[4,i]
+  lines( c(i,i) , pts_in , col=rangi2 )
+  lines( c(i,i)+0.1 , pts_out )
+}
+
