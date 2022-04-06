862d3c6f05f3dd4ca6dcdc475c54ba80a45199fd
icrichmond <icrichmond@mun.ca>
2022-04-06 09:33:32 -0400

    create target for homework 5

---
+++ homework/05-homework-targets.R
@@ -0,0 +1,45 @@
+
+# Targets: Homework 05 ----------------------------------------------------
+
+
+targets_homework05 <- c(
+  # Question 01 -----------------------------------------------------------
+  # data 
+  tar_target(
+    wav_files,
+    dir(folder_path, full.names = TRUE, recursive = TRUE),
+    format = 'file'
+  ),
+
+  # dag 
+  tar_target(
+    
+  ),
+  
+  # total causal binomial GLM
+  tar_target(
+    
+    )
+  ),
+  
+
+  # Question 02 -----------------------------------------------------------
+  
+  # direct effect binomial GLM
+  
+  
+  
+  
+  # Question 03 -----------------------------------------------------------
+  
+  
+  
+  
+  
+  
+  # Question 04 -----------------------------------------------------------
+
+  
+)
+
+
