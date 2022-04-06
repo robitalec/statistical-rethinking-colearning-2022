986a93024b6e39c9ac97a60229217f318cb7433f
icrichmond <icrichmond@mun.ca>
2022-04-06 10:46:47 -0400

    completely finished Ch 11

--- textbook/11-Chapter11.R
+++ textbook/11-Chapter11.R
@@ -233,3 +233,169 @@ lci <- apply(lambda, 2, PI)
 lines(P_seq, lmu, lty=1, lwd=1.5)
 shade(lci, P_seq, xpd=T)
 
+
+# Negative Binomial -------------------------------------------------------
+
+# daily counts
+num_days <- 30
+y <- rpois(num_days, 1.5)
+
+# weekly counts
+num_weeks <- 4
+y_new <- rpois(num_weeks, 0.5*7) # multiply average by 7 since there are 7 days in a week
+
+y_all <- c(y, y_new)
+exposure <- c(rep(1,30), rep(7, 4))
+monastery <- c(rep(0,30), rep(1,4))
+d <- data.frame(y=y_all, days=exposure, monastery = monastery)
+
+# compute offset
+d$log_days <- log(d$days)
+
+# model 
+m11.12 <- quap(alist(
+  y ~ dpois(lambda), 
+  log(lambda) <- log_days + a + b*monastery,
+  a ~ dnorm(0, 1), 
+  b ~ dnorm(0, 1)
+), data = d)
+
+# everything is on daily scale now - don't need offset 
+post <- extract.samples(m11.12)
+lambda_old <- exp(post$a)
+lambda_new <- exp(post$a + post$b)
+precis(data.frame(lambda_old, lambda_new))
+
+
+# Multinomial/Categorical Models  -----------------------------------------
+
+## Predictors Matched to Outcomes
+# simulate career choices among 500 people 
+N <- 500
+income <- c(1,2,5)
+score <- 0.5*income # scores for each career based on income
+# convert scores to probabilities
+p <- softmax(score[1], score[2], score[3])
+
+# simulate choice 
+career <- rep(NA, N)
+# sample chosen career for each individual 
+set.seed(34302)
+for(i in 1:N) careeer[i] <- sample(1:3, size = 1, prob = p)
+
+code_m11.13 <- "
+data{ 
+  int N; // number of individuals 
+  int K; // number of possible careers 
+  int career[N]; //outcome 
+  vector[K] career_income;
+}
+
+parameters{
+  vector[K-1] a; // intercepts
+  real<lower=0> b; //association of income with choice
+}
+
+model{
+  vector[K] p;
+  vector[K] s;
+  a ~ normal(0, 1);
+  b ~ normal(0, 0.5);
+  s[1] = a[1] + b*career_income[1]; 
+  s[2] = a[2] + b*career_income[2];
+  s[3] = 0; // pivot
+  p = softmax(s); 
+  career ~ categorical(p);
+}
+"
+dat_list <- list(N=N, K=3, career=career, career_income=income)
+m11.13 <- stan(model_code = code_m11.13, data = dat_list, chains = 4)
+precis(m11.13, 2)
+
+# conduct counterfactual sim - what happens if we increase income for salary 2?
+post <- extract.samples(m11.13)
+# logit scores
+s1 <- with(post, a[,1] + b*income[1])
+s2_orig <- with(post, a[,2] + b*income[2])
+s2_new <- with(post, a[,2] + b*income[2]*2)
+
+# compute probabilities
+p_orig <- sapply(1:length(post$b), function(i)
+  softmax(c(s1[i],s2_orig[i],0)))
+p_new <- sapply(1:length(post$b), function(i)
+  softmax(c(s1[i],s2_new[i],0)))
+
+# summarize 
+p_diff <- p_new[,2] - p_orig[,2]
+precis(p_diff)
+
+## Predictors Matched to Observations
+
+# simulate family incomes among 500 people 
+N <- 500
+family_income <- runif(N)
+b <- c(-2, 0, 2) # unique coefficient for each type of event
+# simulate choice 
+career <- rep(NA, N)
+# sample chosen career for each individual 
+for(i in 1:N){ 
+  score <- 0.5*(1:3) + b*family_income[i]
+  p <- softmax(score[1], score[2], score[3])
+  careeer[i] <- sample(1:3, size = 1, prob = p)}
+
+code_m11.14 <- "
+data{ 
+  int N; // number of individuals 
+  int K; // number of possible careers 
+  int career[N]; //outcome 
+  real family_income[N];
+}
+
+parameters{
+  vector[K-1] a; // intercepts
+  vector[K-1] b; // coefficients of family income
+}
+
+model{
+  vector[K] p;
+  vector[K] s;
+  a ~ normal(0, 1.5);
+  b ~ normal(0, 1);
+  for(i in 1:N){
+    for(j in 1:(K-1)) s[j] = a[j] + b[j]*family_income[i];
+    s[K] = 0; // the pivot
+    p = softmax(s);
+    career[i] ~ categorical(p);
+  }
+}
+"
+dat_list <- list(N=N, K=3, career=career, family_income=family_income)
+m11.13 <- stan(model_code = code_m11.14, data = dat_list, chains = 4)
+precis(m11.14, 2)
+
+
+# Multinomial as Poisson --------------------------------------------------
+
+data <- UCBadmit
+d <- UCBadmit
+
+# binomial model of admission probability 
+m_binom <- quap(alist(
+  admit ~ dbinom(applications, p),
+  logit(p) <- a,
+  a ~ dnorm(0, 1.5)
+), data = d)
+
+# Poisson model 
+dat <- list(admit = d$admit, rej = d$reject)
+m_pois <- ulam(
+  alist(
+    admit ~ dpois(lambda1),
+    rej ~ dpois(lambda2),
+    log(lambda1) <- a1,
+    log(lambda2) <- a2,
+    c(a1,a2) ~ dnorm(0, 1.5)
+  ), data = dat, chains = 3, cores = 3
+)
+
+inv_logit(coef(m_binom))
