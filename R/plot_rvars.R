60b41daa2ac5546d795b8abf5f89186c0d65218a
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-22 10:27:55 -0700

    error/doesnt translate

--- R/plot_rvars.R
+++ R/plot_rvars.R
@@ -2,5 +2,10 @@ plot_rvars <- function(model, variable, regex = TRUE) {
   rv <- as_draws_rvars(model, variable = variable, regex = regex)[[1]]
   
   ggplot(data.frame(names = rownames(rv), rv)) + 
-    stat_halfeye(aes(xdist = Intercept, y = names))
+    stat_halfeye(aes(xdist = Intercept, y = names)) +
+    theme_minimal()
 }
+
+# plot_rvars(mod_q02, 'r_tank')
+# Error in data.frame(names = rownames(rv), rv) : 
+#   arguments imply differing number of rows: 0, 48
