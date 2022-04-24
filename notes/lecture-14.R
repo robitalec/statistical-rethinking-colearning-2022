5228c385b6efc0f57ec9b10cfeac92d3cd434ffc
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-24 14:00:25 -0700

    broken formulas

--- notes/lecture-14.R
+++ notes/lecture-14.R
@@ -16,22 +16,32 @@ DT[, block := factor(treatment)]
 DT[, actor := factor(actor)]
 
 default <- get_prior(
-  pulled_left ~ (treatment | actor) + (treatment | block),
+  pulled_left ~ treatment + (treatment | actor) + (treatment | block),
   family = bernoulli(),
   data = DT
 )
 
-p <- c(prior(exponential(1), class = sd),
+p <- c(prior(normal(0, 1), class = b),
+       prior(exponential(1), class = sd),
        prior(normal(0, 1), class = Intercept),
        prior(lkj(4), class = cor))
 
+
+# this didnt work (for id 2)
+# pulled_left ~ treatment + (treatment | actor) + (treatment | block),
+# this did...
+# pulled_left ~ treatment + (0 + treatment | actor) + (treatment | block),
+
+
+
 b <- brm(
-  pulled_left ~ (treatment | actor) + (treatment | block),
+  pulled_left ~ treatment + (0 + treatment | actor) + (treatment | block),
   prior = p,
   family = bernoulli(),
   data = DT
 )
 
+b$prior
 
 draws <- data.table(spread_draws(b, r_actor[id,treatment]))
 
@@ -40,5 +50,5 @@ draws[, index_treatment := .GRP, treatment]
 ggplot(draws) + 
   stat_pointinterval(aes(index_treatment, 
                          inv_logit(r_actor))) + 
-  facet_wrap(~index_id) + 
+  facet_wrap(~id) + 
   ylim(0, 1)
