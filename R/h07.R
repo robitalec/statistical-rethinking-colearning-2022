eeff1334db760564094f445e3508f59e2a80bac6
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-25 19:15:28 -0700

    fix use line pred for q01

--- R/h07.R
+++ R/h07.R
@@ -3,15 +3,11 @@
 plot_h07_q01 <- function(model, data) {
   pred <- data.table(add_predicted_draws(data, model))
   
-  pred[, mean_obs := mean(use_contraception), .(district)]
-  pred[, mean_pred := mean(.prediction), .(district)]
+  mean_obs <- data[ .(mean_use = mean(use_contraception)), .(district)]
   
-  ggplot(unique(pred[, .(mean_obs, mean_pred, district)]),
-         aes(x = as.numeric(district))) + 
-    geom_linerange(aes(ymin = mean_obs, ymax = mean_pred),
-                   color = 'darkgrey') + 
-    geom_point(aes(y = mean_obs)) + 
-    geom_point(aes(y = mean_pred), color = 'blue') + 
-    labs(x = 'district', y = 'use_contraception (black: observed, blue: predicted)') + 
+  ggplot(aes(x = as.numeric(district)), data = pred) + 
+    stat_pointinterval(aes(y = .linpred), color = 'blue', alpha = 0.3) +
+    geom_point(aes(y = mean_use), data = mean_obs) + 
+    labs(x = 'district', y = 'use_contraception') + 
     theme_minimal()
 }
