9a97b5e8ff7936f20400307de4d86da4c52f0a7a
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-25 17:49:04 -0700

    fin l14

--- notes/lecture-14.R
+++ notes/lecture-14.R
@@ -15,40 +15,83 @@ DT[, treatment := factor(treatment)]
 DT[, block := factor(treatment)]
 DT[, actor := factor(actor)]
 
+
+## Building up model complexity...
+# Actor only
 default <- get_prior(
-  pulled_left ~ treatment + (treatment | actor) + (treatment | block),
+  pulled_left ~ actor,
   family = bernoulli(),
   data = DT
 )
 
-p <- c(prior(normal(0, 1), class = b),
-       prior(exponential(1), class = sd),
-       prior(normal(0, 1), class = Intercept),
-       prior(lkj(4), class = cor))
-
-
-# this didnt work (for id 2)
-# pulled_left ~ treatment + (treatment | actor) + (treatment | block),
-# this did...
-# pulled_left ~ treatment + (0 + treatment | actor) + (treatment | block),
-
-
+p <- c(prior(exponential(1), class = sd),
+       prior(normal(0, 1), class = Intercept))
 
 b <- brm(
-  pulled_left ~ treatment + (0 + treatment | actor) + (treatment | block),
+  pulled_left ~ (1 | actor),
   prior = p,
+  family = bernoulli(),
+  data = DT,
+  cores = 4,
+  file = 'l14.Rds',
+  file_refit = 'on_change'
+)
+
+mcmc_areas(b, regex_pars = 'r_actor', transformations = inv_logit)
+
+# Actor and block intercepts
+default <- get_prior(
+  pulled_left ~ (1 | actor + block),
   family = bernoulli(),
   data = DT
 )
 
-b$prior
+b2 <- brm(
+  pulled_left ~ (1 | actor + block),
+  prior = p,
+  family = bernoulli(),
+  data = DT,
+  cores = 4,
+  file = 'l14-ab.Rds',
+  file_refit = 'on_change'
+)
 
-draws <- data.table(spread_draws(b, r_actor[id,treatment]))
+mcmc_areas(b2, regex_pars = 'r_actor', transformations = inv_logit)
 
-draws[, index_treatment := .GRP, treatment]
 
-ggplot(draws) + 
-  stat_pointinterval(aes(index_treatment, 
-                         inv_logit(r_actor))) + 
-  facet_wrap(~id) + 
-  ylim(0, 1)
+# Treatment | actor + block
+default <- get_prior(
+  pulled_left ~ treatment + (treatment | actor + block),
+  family = bernoulli(),
+  data = DT
+)
+
+p3 <- c(prior(normal(0, 1.5), class = b),
+        prior(exponential(1), class = sd),
+        prior(normal(0, 1), class = Intercept),
+        prior(lkj(4), class = cor))
+
+b3 <- brm(
+  pulled_left ~ treatment + (treatment | actor)  + (treatment | block),
+  prior = p3,
+  family = bernoulli(),
+  data = DT,
+  cores = 4,
+  file = 'l14-t-ab.Rds',
+  file_refit = 'on_change'
+)
+
+ranef(b3)
+fixef(b3)
+mcmc_areas(b3)
+mcmc_areas(b3, regex_pars = 'r_actor', transformations = inv_logit)
+
+pred <- data.table(add_predicted_draws(DT, b3))
+
+pred[, mean_pred := mean(.prediction), .(actor, treatment, block)]
+pred[, mean_obs := mean(pulled_left), .(actor, treatment, block)]
+
+ggplot(unique(pred[, .(actor, treatment, block, mean_pred, mean_obs)])) + 
+  geom_point(aes(interaction(treatment, actor), mean_obs)) + 
+  geom_point(aes(interaction(treatment, actor), mean_pred), color = 'blue') +
+  theme_minimal()
