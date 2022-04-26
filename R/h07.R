b2a15c3357e91bddf751db5f593780af8193b7ff
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-25 19:17:44 -0700

    fix transform lin pred

--- R/h07.R
+++ R/h07.R
@@ -1,9 +1,9 @@
 ## Question 1
 # Plot predicted district
 plot_h07_q01 <- function(model, data) {
-  pred <- data.table(add_predicted_draws(data, model))
+  pred <- data.table(add_linpred_draws(data, model, transform = TRUE))
   
-  mean_obs <- data[ .(mean_use = mean(use_contraception)), .(district)]
+  mean_obs <- data[, .(mean_use = mean(use_contraception)), .(district)]
   
   ggplot(aes(x = as.numeric(district)), data = pred) + 
     stat_pointinterval(aes(y = .linpred), color = 'blue', alpha = 0.3) +
