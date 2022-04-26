24fb616c141ef3fd31f4d02236ba6140adfa3f0e
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-25 19:25:46 -0700

    model q3 total

--- homework/07-homework-targets.R
+++ homework/07-homework-targets.R
@@ -23,10 +23,22 @@ targets_homework_07 <- c(
     plot_district_contraception,
     plot_h07_q01(h07_q01_brms_sample, DT_bangladesh)
   ),
+  
   # Question 2 --------------------------------------------------------------
-
   
   # Question 3 --------------------------------------------------------------
+  # Model
+  zar_brms(
+    name = h07_q03_total,
+    formula = use_contraception ~ urban + (1 | district),
+    prior = c(
+      prior(normal(0, 1.5), Intercept),
+      prior(normal(0, 1), b),
+      prior(exponential(1), sd)
+    ),
+    data = DT_bangladesh,
+    family = bernoulli()
+  ),
   
   # Render ------------------------------------------------------------------
   tar_render(render_h07,
